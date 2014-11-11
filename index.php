fcc140d30b6e6df24e1aacceb04063175d9ce29e
ruhigit <ruhiagarwal@live.com>
2014-11-11 12:06:42 -0800

    Updated information for charts

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Content-type: text/javascript");
-
     error_reporting(0);
     $streetInput=$_GET["streetInput"];
     $cityInput=$_GET["cityInput"];
@@ -29,7 +28,6 @@ header("Content-type: text/javascript");
 		}
 		$url.="%2C+".$stateInput;
 		$url.="&rentzestimate=true";
-
             $xml=simplexml_load_file($url);
             if($xml->message[0]->code == 0):
             $val1 = number_format((double)$xml->response->results->result->rentzestimate->valuationRange->low, 2);
@@ -41,24 +39,20 @@ header("Content-type: text/javascript");
             $zipcode=(string)$xml->response->results->result->address->zipcode;
             $latitude=(string)$xml->response->results->result->address->latitude;
             $longitude=(string)$xml->response->results->result->address->longitude;
-
             date_default_timezone_set('America/Los_Angeles');
 			if( $xml->response->results->result->useCode!="")
 			$useCode=(string) $xml->response->results->result->useCode; 
 			else 
 			$useCode="N/A";
-
 			if (isset($xml->response->results->result->lastSoldPrice))
 			     $lastSoldPrice=(string) "$".number_format(doubleval($xml->response->results->result->lastSoldPrice), 2);
             else
                 $lastSoldPrice= "N/A";
         
-
             if(isset($xml->response->results->result->yearBuilt))
 			 $yearBuilt=(string) $xml->response->results->result->yearBuilt; 
 			else 
 			 $yearBuilt="N/A";
-
              date_default_timezone_set('America/Los_Angeles') ;
 			if(isset($xml->response->results->result->lastSoldDate))
 			$lastSoldDate=(string) date('d-M-Y',strtotime($xml->response->results->result->lastSoldDate)); 
@@ -74,16 +68,13 @@ header("Content-type: text/javascript");
 			$estimateLastUpdate=(string) date('d-M-Y',strtotime($xml->response->results->result->zestimate->{'last-updated'})).":";
             else 
                 $estimateLastUpdate="N/A";	
-
             if ($xml->response->results->result->zestimate->amount!=""){
 			$estimateAmount=(string)"$".number_format(doubleval($xml->response->results->result->zestimate->amount), 2); 
             }
 			else
 			$estimateAmount= "N/A";
-
             $imgn = "http://cs-server.usc.edu:45678/hw/hw6/down_r.gif";
             $imgp = "http://cs-server.usc.edu:45678/hw/hw6/up_g.gif";
-
             $f=0;
 			if ($xml->response->results->result->zestimate->valueChange!="")
 			{
@@ -97,14 +88,10 @@ header("Content-type: text/javascript");
 				    $estimateValueChangeSign = '+';
 				}
 			}
-
 			if($f)
 			$estimateValueChange = (string) "$".number_format(doubleval(abs($xml->response->results->result->zestimate->valueChange)), 2);
             else{
 				$estimateValueChange ="N/A";}
-
-
-
             if(isset($xml->response->results->result->finishedSqFt))
 			$finishedSqFt=(string) number_format(doubleval($xml->response->results->result->finishedSqFt)) . " sq.ft";
             else
@@ -121,9 +108,9 @@ header("Content-type: text/javascript");
                 $estimateValuationRangeLow ="N/A";
                 
              if($xml->response->results->result->zestimate->valuationRange->high!="")
-			     $estimateValuationRangeHigh = (string)" $". number_format(doubleval($xml->response->results->result->zestimate->valuationRange->high),2);
+			     $estimateValueationRangeHigh = (string)" $". number_format(doubleval($xml->response->results->result->zestimate->valuationRange->high),2);
 			else 
-                $estimateValuationRangeHigh ="N/A";
+                $estimateValueationRangeHigh ="N/A";
                 
                 
             if ($xml->response->results->result->bedrooms!="")
@@ -138,7 +125,6 @@ else $restimateLastUpdate = (string) "N/A";
                 if ($xml->response->results->result->rentzestimate->amount!="")
 			 $restimateAmount = (string)"$".number_format(doubleval($xml->response->results->result->rentzestimate->amount),2);
 else  $restimateAmount = "N/A";
-
             if(isset($xml->response->results->result->taxAssessmentYear))
 			 $taxAssessmentYear = (string) date('Y',strtotime($xml->response->results->result->taxAssessmentYear)); 
 			else 
@@ -165,7 +151,6 @@ else  $restimateAmount = "N/A";
 			     $taxAssessment = (string)"$".number_format(doubleval($xml->response->results->result->taxAssessment), 2);
             else
                  $taxAssessment ="N/A";	    
-
             if ($val1!=0)
 			$restimateValuationRangeLow = (string) "$".$val1;
                 else 
@@ -205,13 +190,12 @@ else  $restimateAmount = "N/A";
                                 'restimateValueChange' => $restimateValueChange,
                                 'taxAssessment' => $taxAssessment,
                                 'restimateValuationRangeLow' => $restimateValuationRangeLow,
-                                'restimateValuationRangeHigh' => $restimateValuationRangeHigh,
-                                'messageCode' => '0');
+                                'restimateValuationRangeHigh' => $restimateValuationRangeHigh
+                                );
                 
-                $zpid = (string)$xml->response[0]->results[0]->result[0]->zpid;
-                        $zwsid = "X1-ZWz1b2xb3nng97_82rpe";
-
-                        $duration='1';
+                $zpid = (string)$xml->response->results->result->zpid;
+                        $zwsid = "X1-ZWz1b2mw7s90y3_5i268";
+                        $duration='1year';
                         $fields = array('zws-id' => $zwsid,
                                         'unit-type' => 'percent',
                                         'zpid' => $zpid,
@@ -224,8 +208,7 @@ else  $restimateAmount = "N/A";
                         //print_r($xmlFile);
                         $urlimage1 = (string)$xmlFile->response[0]->url;
                         $arr1 = array('url' => $urlimage1);
-
-                        $duration='5';
+                        $duration='5years';
                         $fields = array('zws-id' => $zwsid,
                                         'unit-type' => 'percent',
                                         'zpid' => $zpid,
@@ -237,8 +220,7 @@ else  $restimateAmount = "N/A";
                         $xmlFile = simplexml_load_file($url);
                         $urlimage2 =(string)$xmlFile->response[0]->url;
                         $arr2 = array('url' => $urlimage2);
-
-                        $duration='10';
+                        $duration='10years';
                         $fields = array('zws-id' => $zwsid,
                                         'unit-type' => 'percent',
                                         'zpid' => $zpid,
@@ -250,13 +232,12 @@ else  $restimateAmount = "N/A";
                         $xmlFile = simplexml_load_file($url);
                         $urlimage3 = (string)$xmlFile->response[0]->url;
                         $arr3 = array('url' => $urlimage3);
-
                         $imageArr = array(
-                                        '1year' => $arr1,
-                                        '5years' => $arr2,
-                                        '10years' => $arr3);
+                                        'oneyear' => $arr1,
+                                        'fiveyears' => $arr2,
+                                        'tenyears' => $arr3);
                         $finalArray = array('result' => $arr,
-                                        'chart' => $imageArr);
+                                        'chart' => $imageArr,'messageCode' => '0');
                         $json = json_encode($finalArray);
                         echo $json;
                 else:
@@ -264,5 +245,3 @@ else  $restimateAmount = "N/A";
                     $json = json_encode($arr);
                     echo $json;
                 endif;
-//print_r($json);
-?>
